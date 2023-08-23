ae6469c560526202c6e76fa2c7c4dd7e97d89a48
jackmckenzie98 <jack.mckenzie98@outlook.com>
2023-08-23 08:39:17 -0500

    commit

--- prep_post_bodies.py
+++ prep_post_bodies.py
@@ -237,6 +237,7 @@ def prepare_authPolicy_operations():
 def prepare_keyPair_operations():
     cert_list = []
     file_path = os.getcwd()
+    print(f"File path of current working directory: \n{file_path}")
     final_path = os.path.join(file_path, r'artifactsPull')
     cert_path = os.path.join(final_path, r'certs')
     print(f"Files in cert_path: \n{os.listdir(cert_path)}")
