5f4efaf80225737ebe82bc000ab89cb2751d4a6a
jackmckenzie98 <jack.mckenzie98@outlook.com>
2023-08-30 12:58:57 -0500

    Added for encryptedPassword

--- prepare_operation_bodies.py
+++ prepare_operation_bodies.py
@@ -98,13 +98,20 @@ def prepare_operations(entity_type, existing_data, art_data, id_key, data_key, s
             else:
                 PUT_IDs[data_key].append(item[id_key])
                 PUT_Bodies[data_key].append(item)
+
+        # If the body requires a secret to be injected to PUT/POST
         if secret_key is not None:
             for i in range(len(POST_Bodies[data_key])):
                 if POST_Bodies[data_key][i][id_key] != "ProvisionerDS":
                     POST_Bodies[data_key][i] = inject_secret_values(POST_Bodies[data_key][i], "encryptedValue", "value",
                                                                     parse_files.get_secret(secret_key)[secret_key])
+                    POST_Bodies[data_key][i] = inject_secret_values(PUT_Bodies[data_key][i], "encryptedPassword",
+                                                                   "value",
+                                                                   parse_files.get_secret(secret_key)[secret_key])
             for j in range(len(PUT_Bodies[data_key])):
                 PUT_Bodies[data_key][j] = inject_secret_values(PUT_Bodies[data_key][j], "encryptedValue", "value",
+                                                              parse_files.get_secret(secret_key)[secret_key])
+                PUT_Bodies[data_key][j] = inject_secret_values(PUT_Bodies[data_key][j], "encryptedPassword", "value",
                                                               parse_files.get_secret(secret_key)[secret_key])
 
     # Handle the case that we're PUT-ing the auth policy since its object is a bit unique.
