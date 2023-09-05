4580e937fd582a54fc759c01a69039e072d344a4
jackmckenzie98 <jack.mckenzie98@outlook.com>
2023-09-05 16:55:51 -0500

    Add a try except to catch unsuccessful error codes and print them out as a means of catching bad requests

--- execute_operations.py
+++ execute_operations.py
@@ -47,27 +47,30 @@ def execute_calls():
     for key, val in prepare_operation_bodies.PUT_Bodies.items():
         #Run POST operations then PUTs
         print(f'\n\n\n\nOperations running now on {key}...\n')
-        for i in range(0, len(prepare_operation_bodies.POST_Bodies[key])):
-            json_body = json.loads(json.dumps(prepare_operation_bodies.POST_Bodies[key][i]))
-            response = session.post(url=f'{url}{endpoints[key]}', json=json_body)
-            print(f'Response code for POST to {url}{endpoints[key]} is {response.status_code} for call'
-                  f' made with following JSON:\n {json_body}\n')
-            print(f'Response body is as follows:\n {response.content}\n\n\n')
-        if key not in ('authPolicies', 'OAuthKeys', 'virtualHosts', 'redirectValidation'):
-            for i in range(0, len(prepare_operation_bodies.PUT_Bodies[key])):
-                json_body = json.loads(json.dumps(prepare_operation_bodies.PUT_Bodies[key][i]))
-                response = session.put(url=f'{url}{endpoints[key]}/{prepare_operation_bodies.PUT_IDs[key][i]}',
-                                       json=json_body)
-                print(f'Response code for PUT to {url}{endpoints[key]}/{prepare_operation_bodies.PUT_IDs[key][i]} is '
-                      f'{response.status_code} with the following JSON:\n {json_body}\n')
+        try:
+            for i in range(0, len(prepare_operation_bodies.POST_Bodies[key])):
+                json_body = json.loads(json.dumps(prepare_operation_bodies.POST_Bodies[key][i]))
+                response = session.post(url=f'{url}{endpoints[key]}', json=json_body)
+                print(f'Response code for POST to {url}{endpoints[key]} is {response.status_code} for call'
+                      f' made with following JSON:\n {json_body}\n')
                 print(f'Response body is as follows:\n {response.content}\n\n\n')
-        else:
-            for j in range(0, len(prepare_operation_bodies.PUT_Bodies[key])):
-                json_body = json.loads(json.dumps(prepare_operation_bodies.PUT_Bodies[key][j]))
-                response = session.put(url=f'{url}{endpoints[key]}', json=json_body)
-                print(f'Response code for PUT to {url}{endpoints[key]} is'
-                      f' {response.status_code} with the following JSON:\n {json_body}\n')
-                print(f'Response body is as follows:\n {response.content}\n\n\n')
+            if key not in ('authPolicies', 'OAuthKeys', 'virtualHosts', 'redirectValidation'):
+                for i in range(0, len(prepare_operation_bodies.PUT_Bodies[key])):
+                    json_body = json.loads(json.dumps(prepare_operation_bodies.PUT_Bodies[key][i]))
+                    response = session.put(url=f'{url}{endpoints[key]}/{prepare_operation_bodies.PUT_IDs[key][i]}',
+                                           json=json_body)
+                    print(f'Response code for PUT to {url}{endpoints[key]}/{prepare_operation_bodies.PUT_IDs[key][i]} is '
+                          f'{response.status_code} with the following JSON:\n {json_body}\n')
+                    print(f'Response body is as follows:\n {response.content}\n\n\n')
+            else:
+                for j in range(0, len(prepare_operation_bodies.PUT_Bodies[key])):
+                    json_body = json.loads(json.dumps(prepare_operation_bodies.PUT_Bodies[key][j]))
+                    response = session.put(url=f'{url}{endpoints[key]}', json=json_body)
+                    print(f'Response code for PUT to {url}{endpoints[key]} is'
+                          f' {response.status_code} with the following JSON:\n {json_body}\n')
+                    print(f'Response body is as follows:\n {response.content}\n\n\n')
+        except requests.exceptions.RequestException as e:
+            print('Unexpected result code for: ', e)
 
 execute_calls()
 
